04d9e0382094117ab83122792be73dd308bba93b
Alejandra acevedo <urregoaleja10@gmail.com>
2019-03-12 10:52:17 +0100

    Premier view

---
+++ templates/headTemplate.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Site de Quiz en Ligne IMT</title>
+<link rel="stylesheet" href="styles/style.css" type="text/css" media="screen" title="style1" charset="utf-8" />
+</head>
+<body>
+<div id="page">
