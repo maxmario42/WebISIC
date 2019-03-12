05f527ff8c6aaac141f36eca5bd1047130af59dd
maxmario42 <38248353+maxmario42@users.noreply.github.com>
2019-03-12 11:00:03 +0100

    Update

--- view/View.class.php
+++ view/View.class.php
@@ -1,26 +1,29 @@
-<?php
-
-class View extends MyObject
-{
+<?php 
+/* 
+* Root class of all my classes 
+*/
+class View extends MyObject { 
+    //On declare les objets a utiliser pour notre view.
     protected $controller;
     protected $args;
     protected $templateName;
 
-    public function __construct(Controller $controller, $templateName = 'home', $args = array())
+    public function __construct(Controller $controller, $templateName ='home', $args= array())
     {
         $this->controller = $controller;
         $this->templateName = $templateName;
         $this->args = $args;
     }
 
-    public function render()
-    {
-        $this->getTemplate('layout/head');
-        $this->getTemplate('layout/menu');
-        $this->getTemplate('page/'.$this->templateName);
-        $this->getTemplate('layout/foot');
+    public function render(){
+        $this->getTemplate('head');
+        //$this->getTemplate('top');
+       // $this->getTemplate('menu');
+        $this->getTemplate($this->templateName);
+        //$this->getTemplate('foot'); 
+
     }
-    
+
     protected function getTemplate($templateName)
     {
         include_once(implode('/', array(
@@ -29,6 +32,5 @@ class View extends MyObject
             $templateName.'Template.php'
         )));
     }
-}
-
-?>
+} 
+?> 
