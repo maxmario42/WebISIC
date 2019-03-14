7621d50aad9e3c99f14caabf2a00ac7ca00ae96f
Alejandra acevedo <urregoaleja10@gmail.com>
2019-03-14 18:40:59 +0100

    final change

--- templates/homeTemplate.php
+++ templates/homeTemplate.php
@@ -7,20 +7,28 @@
             <div class="home">
             </div>
         
-                <div class="jumbotron">
+			<div class="jumbotron">
+				<div class="container h-100">
+           		<div class="row h-100 align-items-center">
+                <div class="col-12">
+
                 
                         <h1>Bienvenue</h1>
                         <p>Vous êtes sur notre site de QUIZ en Ligne, une site creé par des étudiants de l'IMT Lille Douai !</p>
                     
                         <p>N'hésitez pas à créer un compte pour profiter de nos services :</p>
                         
-                
-                    
+						<div class="container">
+  			
+			 		 	<p><a href="#" class="btn btn-info" role="button">S'inscrire</a><p>
+						</div>
+						</div>
+				</div>
+				</div>
+				</div>
+ 
 				</div>
 				
-				<div class="container">
-  			
-					  <p><a href="#" class="btn btn-info" role="button">S'inscrire</a><p>
-			</div>
+				
             
         </main>
