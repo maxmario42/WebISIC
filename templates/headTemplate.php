0279a5e89a9a15ee18af615a8b67e5c38a96bdb9
Alejandra acevedo <urregoaleja10@gmail.com>
2019-03-14 17:48:25 +0100

    nouvelle view avec elements

--- templates/headTemplate.php
+++ templates/headTemplate.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!--<!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -6,4 +6,31 @@
 <link rel="stylesheet" href="styles/style.css" type="text/css" media="screen" title="style1" charset="utf-8" />
 </head>
 <body>
-<div id="page">
+<div id="page"> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *Must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>Site de Quiz en Ligne pour L'IMT Lille - Douai | Home</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="assets/quiz1.ico" type="image/x-icon">
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+</head>
+<body>
+    <div id="page">
