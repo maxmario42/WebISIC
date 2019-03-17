7f3847a1c411ea0c06dbb1dda2d31c72171715cc
Alejandra acevedo <urregoaleja10@gmail.com>
2019-03-17 20:58:18 +0100

    change inscription

--- controller/AnonymousController.class.php
+++ controller/AnonymousController.class.php
@@ -16,7 +16,30 @@ class AnonymousController extends Controller
        $view->render();  
     }
 
-    
+    public function validateInscription($request) {
+        $login = $request->read('inscLogin');
+        if(User::isLoginUsed($login)) {
+        $view = new View($this,'inscription');
+        $view->setArg('inscErrorText','This login is already used');
+        $view->render();
+        } else {
+        $password = $request->read('inscPassword');
+        $nom = $request->read('nom');
+        $prenom = $request->read('prenom');
+        $mail_etudiant = $request->read('mail');
+        $user = User::create($nom, $prenom, $type_utilisateur='Invite', $matricule=NULL, $statut=NULL, $mail_enseignant=NULL, $promo=NULL, $annee_de_sortie=NULL, $mail_etudiant, $password);
+        if(!isset($user)) {
+        $view = new View($this,'inscription');
+        $view->setArg('inscErrorText', 'Cannot complete inscription');
+        $view->render();
+        } else {
+        $newRequest = new Request();
+        $newRequest->write('controller','user');
+        $newRequest->write('user',$user->id());
+        Dispatcher::dispatch($newRequest);
+        }
+        }
+        }
 
    /*function __construct($request) {
       parent::  _construct($request);
--- templates/inscriptionTemplate.php
+++ templates/inscriptionTemplate.php
@@ -6,24 +6,28 @@ echo '<span class="error">' . $inscErrorText . '</span>';
 <form action="index.php" method="post">
 <table>
 <tr>
-<th>Login* :</th>
-<td><input type="text" name="inscLogin"/></td>
+<th>mail* :</th>
+<td><input type="text" name="mail"/></td>
 </tr>
 <tr>
 <th>Mot de passe* :</th>
 <td><input type="password" name="inscPassword"/></td>
 </tr>
 <tr>
-<th>Mail :</th>
-<td><input type="text" name="mail"/></td>
-</tr>
-<tr>
-<th>Nom :</th>
+<th>Nom:</th>
 <td><input type="text" name="nom"/></td>
 </tr>
 <tr>
 <th>Prenom :</th>
 <td><input type="text" name="prenom"/></td>
+</tr>
+<tr>
+<th>Promo :</th>
+<td><input type="text" name="promo"/></td>
+</tr>
+<tr>
+<th>Anne de sortie:</th>
+<td><input type="text" name="anne-sortie"/></td>
 </tr>
 <tr>
 <th>
