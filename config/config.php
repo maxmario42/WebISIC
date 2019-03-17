c5758db2eecaf86d019f1fff63ccb0113c7484b6
Alejandra acevedo <urregoaleja10@gmail.com>
2019-03-17 21:59:34 +0100

    test bd

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 define('_MYSQL_HOST','localhost');
 define('_MYSQL_PORT',3306);	
-define('_MYSQL_DBNAME','dracar');
+define('_MYSQL_DBNAME','test_bd');
 define('_MYSQL_USER','root');
 define('_MYSQL_PASSWORD','root');
 ?>
