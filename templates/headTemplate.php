f1540dee245d8337dcd6590ed9e8e7b54084beae
maxmario42 <38248353+maxmario42@users.noreply.github.com>
2019-03-18 21:43:03 +0100

    Update headTemplate.php

--- templates/headTemplate.php
+++ templates/headTemplate.php
@@ -9,7 +9,7 @@
 <div id="page"> -->
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -27,10 +27,10 @@
     <!-- Stylesheet -->
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/3.4.0/bootstrap.min.css">
+    <script src="js/jquery/jquery.min.js"></script>
+    <script src="bootstrap/3.4.0/bootstrap.min.js"></script>
+    <link href="css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
 </head>
 <body>
