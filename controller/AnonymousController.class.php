5584bf1f1eb153cd4b1b9d14a990541bfb458679
maxmario42 <38248353+maxmario42@users.noreply.github.com>
2019-03-19 08:56:48 +0100

    Update AnonymousController

--- controller/AnonymousController.class.php
+++ controller/AnonymousController.class.php
@@ -31,6 +31,28 @@ class AnonymousController extends Controller
       $view->render();
     }
 
+    public function validateloginAction()
+    {
+        if ($this->request->getUser()!=null) {
+            $this->redirect(Router::path('profile/login'));
+        } else {
+            if ($this->request->isPost()) {
+                $login = $this->request->POST('inscLogin');
+                $password = $this->request->POST('inscPassword');
+                $user = User::findOneBy(array('LOGIN' => $login));
+                if ($user->getPassword() == $password) {
+                    $this->request->setUser($user);
+                    $this->redirect(Router::path('profile/login'));
+                } else {
+                    $view = new View($this, 'profile/login');
+                }
+            } else {
+                $view = new View($this, 'profile/login');
+            }
+        }
+        $view->render();
+    }
+
     public function validateInscriptionAction($request) {
        
       $login = $request->read('inscLogin');
@@ -42,7 +64,6 @@ class AnonymousController extends Controller
         $view->render();
          }  
        else {
-         $login = $request->read('inscLogin');
         $password = $request->read('inscPassword');
         $nom = $request->read('nom');
         $prenom = $request->read('prenom');
