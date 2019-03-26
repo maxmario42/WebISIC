6b4daf7537c89aa9b356cf550beb205bdf72fecf
maxmario42 <38248353+maxmario42@users.noreply.github.com>
2019-03-26 08:35:13 +0100

    Ménage

--- classes/Dispatcher.class.php
+++ classes/Dispatcher.class.php
@@ -20,26 +20,3 @@ fonction des paramètres de la requête.
 	}
 }
 ?>
-<?php
-/*
-class Dispatcher extends MyObject
-{
-    /*
-    Le dispatcher courant doit permettre d’aiguiller la requête courante sur le bon contrôleur en
-fonction des paramètres de la requête.
-    */
-    /*
-    public static function dispatch(Request $request)
-    {
-        $controllerName = $request->getControllerName();
-        //controllerClassName= ucfirst($controllerName.'Controller');
-        return new $controllerName($request);
-
-        /*if class_exists($controllerClassName){
-                throw exception ('class does not exist')
-    }*/
-    /*
-    }
-}
-*/
-?>
--- controller/Controller.class.php
+++ controller/Controller.class.php
@@ -9,7 +9,7 @@ abstract class Controller extends MyObject {
     Abstract function defaultAction($currentRequest); //Action par défaut
 
     public function execute(){
-        //Exécute l'action demandée (celle par défaut si pas de demande)
+        //Appelle la bonne méthode du contrôleur i.e soit l’action par défaut (defaultAction), soit l’action dont le nom a été passé dans la requête courante.
         if(is_null($this->request->getAction())){
             $this->defaultAction($this->request);
         }
@@ -26,31 +26,4 @@ abstract class Controller extends MyObject {
         exit();
     }
 }
-?>
-
-<?php
-/*
-abstract class Controller extends MyObject
-{
-    protected $request;
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-    public function execute()
-    //Appelle la bonne méthode du contrôleur i.e soit l’action par défaut (defaultAction), soit l’action dont le nom a été passé dans la requête courante.
-    { 
-        $action = $this->request->getActionName();
-        $this->$action($this->request);
-    }
-    abstract public function defaultAction($request); //action par défaut du contrôleur. Une action peut nécessiter des paramètres qui doivent être présents dans la requête courante.
-
-
-    public function redirect($url)
-    {
-        header('Location: '.$url);
-        exit();
-    }
-}
-*/
 ?>
