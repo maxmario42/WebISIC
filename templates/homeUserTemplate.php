7035ec60b2d2ae66872a0334703c68814d1b40a3
Dorine kepseu <keps_mido@yahoo.fr>
2019-03-31 15:25:27 +0200

    testrecup

---
+++ templates/homeUserTemplate.php
@@ -0,0 +1,30 @@
+
+	<main>
+            <div class="home">
+            </div>
+        
+			<div class="jumbotron">
+				<div class="container h-100">
+           		<div class="row h-100 align-items-center">
+                <div class="col-12">
+
+                
+                        <h1>Bienvenue</h1>
+                         <p> vous etes bien connecté<p>      
+                        <p>cliquez pour derouler le menu </p>
+												
+    
+						<div class="container"> 
+  			
+
+						</div>
+						</div>
+				</div>
+				</div>
+				</div>
+ 
+				</div>
+				
+				
+            
+        </main>
