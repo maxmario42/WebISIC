ab3797e7750f754be50b9662f596bc8154a21934
Alejandra acevedo <urregoaleja10@gmail.com>
2019-04-01 17:28:09 +0200

    createQuestionnaire

--- model/Questionnaire.class.php
+++ model/Questionnaire.class.php
@@ -2,8 +2,10 @@
 
 Class Questionnaire extends Model{
 
-   // public function create($){
-
+   public function create($titre, $description, $etat,$date_o,$date_f,$mode_access,$lien_http ){
+        /*
+    INSERT INTO `QUESTIONNAIRE`(`IDQ`, `ID`, `TITRE`, `ID_REGLES_QUEST`, `DESCRIPTION`, `ETAT`, `DATE_OUVERTURE`, `DATE_FERMETURE`, `MODE_ACCES`, `LIEN_HTTP`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10])
+        */
     }
 
 }
