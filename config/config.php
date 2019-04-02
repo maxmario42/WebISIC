74fbff91bd552bed0570f77e34a104219ec6b626
maxmario42 <38248353+maxmario42@users.noreply.github.com>
2019-04-02 15:49:10 +0200

    Supression pour gitignore

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('_MYSQL_HOST','localhost');
-define('_MYSQL_PORT',3306);	
-define('_MYSQL_DBNAME','test_bd'); // 
-define('_MYSQL_USER','root'); //root
-define('_MYSQL_PASSWORD','root'); //root
-?>
