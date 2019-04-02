6d9227938c0857bc6e8d7d087e21629797a3130f
maxmario42 <38248353+maxmario42@users.noreply.github.com>
2019-04-02 15:40:57 +0200

    gitignore

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 define('_MYSQL_HOST','localhost');
-define('_MYSQL_PORT',3306);	
-define('_MYSQL_DBNAME','test_bd'); // 
-define('_MYSQL_USER','root'); //root
-define('_MYSQL_PASSWORD','root'); //root
+define('_MYSQL_PORT',3307);	
+define('_MYSQL_DBNAME','quizmines'); // 
+define('_MYSQL_USER','quizmines'); //root
+define('_MYSQL_PASSWORD','quizmines'); //root
 ?>
