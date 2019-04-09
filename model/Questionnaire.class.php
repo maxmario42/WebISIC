53861154687451a37f523955ac6a5817da923a99
Alejandra acevedo <urregoaleja10@gmail.com>
2019-04-09 11:25:15 +0200

    saving

--- model/Questionnaire.class.php
+++ model/Questionnaire.class.php
@@ -2,10 +2,36 @@
 
 Class Questionnaire extends Model{
 
-   public function create($titre, $description, $etat,$date_o,$date_f,$mode_access,$lien_http ){
-        /*
-    INSERT INTO `QUESTIONNAIRE`(`IDQ`, `ID`, `TITRE`, `ID_REGLES_QUEST`, `DESCRIPTION`, `ETAT`, `DATE_OUVERTURE`, `DATE_FERMETURE`, `MODE_ACCES`, `LIEN_HTTP`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10])
-        */
+    public static function getTableName()
+    {
+        return 'QUESTIONNAIRE';
+    }
+
+    public static function getColumns()
+    {
+        return array(
+            'IDQ',
+            'ID',
+            'TITRE',
+            'ID_REGLES_QUEST',
+            'DESCRIPTION',
+            'ETAT',
+            'DATE_OUVERTURE',
+            'DATE_FERMETURE',
+            'MODE_ACCES',
+            'LIEN_HTTP',
+            
+        );
+    }
+
+    public static function getIDColumn()
+    {
+        return 'IDQ';
+    }
+
+   public function create($id,$titre, $description, $etat,$date_o,$date_f,$mode_access,$lien_http ){
+    static::db()->exec("INSERT INTO QUESTIONNAIRE (id,titre,id_regles_quest,description,etat,date_ouverture,date_fermeture,mode_acces,lien_http) VALUES ($id,'$titre',1,'$description','$etat','$date_o','$date_f','$mode_access','$lien_http')");
+    return static::showQuest($titre);
     }
 
 
