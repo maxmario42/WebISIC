ef605a4653ac1ffcd6fe59a7baae9a6f987b9a2e
Dorine kepseu <keps_mido@yahoo.fr>
2019-04-10 18:47:16 +0200

    test1

--- templates/questionnaire/creerQuestionnaireTemplate.php
+++ templates/questionnaire/creerQuestionnaireTemplate.php
@@ -140,7 +140,7 @@
             </div>
         </div>
         
-        <button type="submit" class="btn btn-warning text-dark"> Creer mon Questionnaire </button>
+        <button type="submit" class="btn btn-warning  text-dark"> Creer mon Questionnaire </button>
         </form>
 
     </div>
