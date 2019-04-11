52382e551f324c6a7a85377da0e3d860770756de
maxmario42 <38248353+maxmario42@users.noreply.github.com>
2019-04-11 18:06:09 +0200

    Oups

--- model/Question.class.php
+++ model/Question.class.php
@@ -25,7 +25,7 @@ Class Question extends Model{
     public static function create($idq, $typeq, $temps_max)
     {
         static::db()->exec("INSERT INTO QUESTIONNAIRE (INTITULE,TYPEQ,TEMPS_MAXIMAL) VALUES ($idq,'$typeq',$temps_max)");
-        static::db()->exec("INSERT INTO AJOUTER (IDQ,ID_QUEST) VALUES ($idq,".PDO::lastInsertId".)");
+        static::db()->exec("INSERT INTO AJOUTER (IDQ,ID_QUEST) VALUES ($idq,".PDO::lastInsertId.")");
         return static::getWithId($idq);
     }
 }
