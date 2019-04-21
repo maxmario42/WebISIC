a9dce9a16ecee2ffbf5110fa06bd34675c2afa59
maxmario42 <38248353+maxmario42@users.noreply.github.com>
2019-04-21 17:55:43 +0200

    Retour Erreur

--- templates/errorTemplate.php
+++ templates/errorTemplate.php
@@ -3,6 +3,7 @@
 		<div style="text-align: center">
 			<h1><?php echo $this->getArg('error')->getCode(); ?></h1>
 			<p><?php echo $this->getArg('error')->getMessage() ?: 'Erreur inconnue'; ?></p>
+			<p><a href="<?php echo $this->linkTo(NULL); ?>">Retour à l'acceuil</a></p>
 		</div>
 	</main>
 </div>
