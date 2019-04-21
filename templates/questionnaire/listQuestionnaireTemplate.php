b456a991218c82f494dde42db076642471517d20
maxmario42 <38248353+maxmario42@users.noreply.github.com>
2019-04-21 18:33:41 +0200

    Gestion du vide

--- templates/questionnaire/listQuestionnaireTemplate.php
+++ templates/questionnaire/listQuestionnaireTemplate.php
@@ -1,3 +1,25 @@
+<?php if (empty($this->getArg('questionnaires'))) : ?>
+    <div main class="container" style="flex-grow:1;">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 jumbotron py-3">
+
+                <div class="text-center">
+                    <h2>C'est un peu vide ici...</h2>
+                </div>
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Il faut créer un questionnaire !</th>
+                            <td>Pour cela, cliquez sur le bouton</td>
+                        <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Questionnaire'); ?>';">
+                            <span class="fa fa-plus-circle"></span> Créer un questionnaire
+                        </button>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+<?php else : ?>
 <?php foreach ($this->getArg('questionnaires') as $questionnaires) : ?>
     <div main class="container" style="flex-grow:1;">
         <div class="row justify-content-center">
@@ -25,3 +47,4 @@
         </div>
     </div>
 <?php endforeach ?>
+<?php endif ?>
