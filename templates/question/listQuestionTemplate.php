4d1b76d5d1063b134baa7d6b7ebfb12c1dd031f8
maxmario42 <38248353+maxmario42@users.noreply.github.com>
2019-04-23 09:07:19 +0200

    Boutons Gestions

--- templates/question/listQuestionTemplate.php
+++ templates/question/listQuestionTemplate.php
@@ -11,59 +11,53 @@
                         <tr>
                             <th scope="row">Il faut créer une question !</th>
                             <td>Pour cela, cliquez sur le bouton</td>
-                        <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Question', 'defaultAction', array('idq' => $questionnaire->IDQ)); ?>';">
-                            <span class="fa fa-plus-circle"></span> Créer une question
-                        </button>
+                            <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Question', 'defaultAction', array('idq' => $questionnaire->IDQ)); ?>';">
+                                <span class="fa fa-plus-circle"></span> Créer une question
+                            </button>
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
 <?php else : ?>
-    <?php foreach ($questions as $question) : ?>
+    <?php foreach ($questions as $unequestion) : ?>
         <div main class="container" style="flex-grow:1;">
             <div class="row justify-content-center">
                 <div class="col-lg-6 jumbotron py-3">
 
                     <div class="text-center">
-                        <h2><?php echo $question->INTITULE; ?></h2>
+                        <h2><?php echo $unequestion->INTITULE; ?></h2>
                     </div>
                     <table class="table">
                         <tbody>
                             <tr>
                                 <th scope="row">Intitule</th>
-                                <td><?php echo $question->INTITULE; ?></td>
+                                <td><?php echo $unequestion->INTITULE; ?></td>
                             </tr>
                             <tr>
                                 <th scope="row">TypeQ</th>
-                                <td><?php echo $question->TYPEQ; ?></td>
+                                <td><?php echo $unequestion->TYPEQ; ?></td>
                             </tr>
                             <tr>
                                 <th scope="row">Temps_Max</th>
-                                <td><?php echo $question->TEMPS_MAX; ?></td>
+                                <td><?php echo $unequestion->TEMPS_MAX; ?></td>
                             </tr>
                             <tr>
                                 <th scope="row">Tag</th>
-                                <td><?php echo $question->TAG; ?></td>
+                                <td><?php echo $unequestion->TAG; ?></td>
                             </tr>
 
-                            <?php 
-                           /*
-                            <?php if (isset($question)) : ?>
-                                      <a class="aMenu" href="<?php echo $this->linkTo('Question', 'showQuestion', array('idq' => $questionnaire->IDQ, 'idquest' => $question->ID_QUEST)); ?>"><?php echo $question->INTITULE; ?></a>
-                                          <ul class="dropdown">
-                                              <li><a href="<?php echo $this->linkTo('Question', 'edit', array('idq' => $questionnaire->IDQ, 'idquest' => $question->ID_QUEST)); ?>"><span class="fa fa-pencil-square-o"></span> Edition</a></li>
-                                              <li><a href="<?php echo $this->linkTo('Question', 'deleteQuestion', array('idq' => $questionnaire->IDQ, 'idquest' => $question->ID_QUEST)); ?>"><span class="fa fa-times"></span> Supprimer</a></li>
-                                          </ul>
-                            <?php endif ?>
-                            */
-                            ?>
-                            <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Question', 'showQuestion', array('idq' => $questionnaire->IDQ, 'idquest' => $question->ID_QUEST)); ?>';">
-                                <span class="fa fa-search"></span> Détail
-                            </button><!--
-                            <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Question', 'deleteQuestion', array('idq' => $questionnaire->IDQ, 'idquest' => $question->ID_QUEST)); ?>';">
-                                <span class="fa fa-times"></span> Supprimer
-                            </button>-->
+                            <div class="btn-group">
+                                <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Question', 'showQuestion', array('idq' => $questionnaire->IDQ, 'idquest' => $unequestion->ID_QUEST)); ?>';">
+                                    <span class="fa fa-search"></span> Détail
+                                </button>
+                                <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Question', 'edit', array('idq' => $questionnaire->IDQ, 'idquest' => $unequestion->ID_QUEST)); ?>';">
+                                    <span class="fa fa-pencil-square-o"></span> Edition
+                                </button>
+                                <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Question', 'deleteQuestion', array('idq' => $questionnaire->IDQ, 'idquest' => $unequestion->ID_QUEST)); ?>';">
+                                    <span class="fa fa-times"></span> Supprimer
+                                </button>
+                            </div>
                         </tbody>
                     </table>
                 </div>
--- templates/questionnaire/listQuestionnaireTemplate.php
+++ templates/questionnaire/listQuestionnaireTemplate.php
@@ -21,81 +21,46 @@
     </div>
 <?php else : ?>
 
-<?php foreach ($questionnaires as $unquestionnaire) : ?>
-    <div main class="container" style="flex-grow:1;">
-        <div class="row justify-content-center">
-            <div class="col-lg-6 jumbotron py-3">
+    <?php foreach ($questionnaires as $unquestionnaire) : ?>
+        <div main class="container" style="flex-grow:1;">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 jumbotron py-3">
 
-                <div class="text-center">
-                    <h2><?php echo $unquestionnaire->TITRE; ?></h2>
-                </div>
-                <table class="table">
-                    <tbody>
-                        <tr>
-                            <th scope="row">Titre</th>
-                            <td><?php echo $unquestionnaire->TITRE; ?></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Description</th>
-                            <td><?php echo $unquestionnaire->DESCRIPTION; ?></td>
-                        </tr>
-
-                        <?php if (isset($unquestionnaire)) : ?>
-                                      
-                                      <a class="aMenu" href="<?php echo $this->linkTo('Questionnaire', 'showQuiz', array('idq' => $unquestionnaire->IDQ)); ?>"><?php echo $unquestionnaire->TITRE; ?></a>
-                                          <ul class="dropdown">
-                                              <li><a href="<?php echo $this->linkTo('Question', 'defaultAction', array('idq' => $unquestionnaire->IDQ)); ?>"><span class="fa fa-plus-circle"></span> Créer Question</a></li>
-                                              <li><a href="<?php echo $this->linkTo('Question', 'showListQuestion', array('idq' => $unquestionnaire->IDQ)); ?>"><span class="fa fa-search"></span> Voir les Questions</a></li>
-                                              <li><a href="<?php echo $this->linkTo('Questionnaire', 'edit', array('idq' => $unquestionnaire->IDQ)); ?>"><span class="fa fa-pencil-square-o"></span> Edition</a></li>
-                                              <li><a href="<?php echo $this->linkTo('Questionnaire', 'deleteQuest', array('idq' => $unquestionnaire->IDQ)); ?>"><span class="fa fa-times"></span> Supprimer</a></li>
-                                          </ul>
-                                      
-                                      
-                        <?php endif ?>
-
-                        <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Questionnaire', 'showQuiz', array('idq' => $unquestionnaire->IDQ)); ?>';">
-                            <span class="fa fa-search"></span>Détail
-                        </button>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-
-<!-- ##### Popular Course Area Start ##### -->
-    <section class="popular-courses-area section-padding-100">
-        <div class="container">
-            <div class="row">
-                <?php foreach ($questionnaires as $unquestionnaire) : ?>
-                    <!-- Single Popular Course -->
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="single-popular-course mb-100 wow fadeInUp" data-wow-delay="250ms">
-                            <!-- Course Content -->
-                            <div class="course-content">
-                                <h4><?php echo $unquestionnaire->TITRE; ?></h4>
-                                <div class="meta d-flex align-items-center">
-                                    <a href="#"><?php echo User::getWithId($unquestionnaire->ID)->PRENOM;?> <?php echo User::getWithId($unquestionnaire->ID)->NOM;?></a>
-                                </div>
-                                <p><?php echo $unquestionnaire->DESCRIPTION; ?></p>
-                            </div>
-                            <!-- Seat Rating Fee -->
-                            <div class="seat-rating-fee d-flex justify-content-between">
-                                <div class="seat-rating h-100 d-flex align-items-center">
-                                    <div class="seat">
-                                        <i class="fa fa-user" aria-hidden="true"></i> 10
-                                    </div>
-                                </div>
-                                <div class="course-fee h-100">
-                                    <a href="<?php echo $this->linkTo('Questionnaire', 'showQuiz', array('idq' => $unquestionnaire->IDQ)); ?>" class="free">Détails</a>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="text-center">
+                        <h2><?php echo $unquestionnaire->TITRE; ?></h2>
                     </div>
-                <?php endforeach ?>
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <th scope="row">Titre</th>
+                                <td><?php echo $unquestionnaire->TITRE; ?></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Description</th>
+                                <td><?php echo $unquestionnaire->DESCRIPTION; ?></td>
+                            </tr>
 
+                            <div class="btn-group">
+                                <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Questionnaire', 'showQuiz', array('idq' => $unquestionnaire->IDQ)); ?>';">
+                                    <span class="fa fa-search"></span> Détail
+                                </button>
+                                <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Question', 'defaultAction', array('idq' => $unquestionnaire->IDQ)); ?>';">
+                                    <span class="fa fa-plus-circle"></span> Créer Question
+                                </button>
+                                <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Question', 'showListQuestion', array('idq' => $unquestionnaire->IDQ)); ?>';">
+                                    <span class="fa fa-search"></span> Voir les Questions
+                                </button>
+                                <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Questionnaire', 'edit', array('idq' => $unquestionnaire->IDQ)); ?>';">
+                                    <span class="fa fa-pencil-square-o"></span> Edition
+                                </button>
+                                <button class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '<?php echo $this->linkTo('Questionnaire', 'deleteQuest', array('idq' => $unquestionnaire->IDQ)); ?>';">
+                                    <span class="fa fa-times"></span> Supprimer
+                                </button>
+                            </div>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </section>
-<?php endforeach ?>
+    <?php endforeach ?>
 <?php endif ?>
-<!-- ##### Popular Course Area End ##### -->
